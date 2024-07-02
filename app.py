3db3907e7b50e68a534d045379203aa6db74d689
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2024-07-02 16:03:44 +0600

    Unneceassary file removed

--- app.py
+++ app.py
@@ -10,39 +10,41 @@ pipe = pickle.load(open('pipe.pkl', 'rb'))
 st.title('Laptop Price Analysis')
 
 # brand
-company = st.selectbox('Brand',df['Company'].unique())
+company = st.selectbox('Brand', df['Company'].unique())
 
 # type of laptop
-type = st.selectbox('Type',df['TypeName'].unique())
+type = st.selectbox('Type', df['TypeName'].unique())
 
 # Ram
-ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+ram = st.selectbox('RAM(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
 
 # weight
 weight = st.number_input('Weight of the Laptop')
 
 # Touchscreen
-touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
 
 # IPS
-ips = st.selectbox('IPS',['No','Yes'])
+ips = st.selectbox('IPS', ['No', 'Yes'])
 
 # screen size
 screen_size = st.number_input('Screen Size')
 
 # resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+resolution = st.selectbox('Screen Resolution',
+                          ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600',
+                           '2560x1440', '2304x1440'])
 
-#cpu
-cpu = st.selectbox('CPU',train['Processor'].unique())
+# cpu
+cpu = st.selectbox('CPU', train['Processor'].unique())
 
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
 
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
 
-gpu = st.selectbox('GPU',train['Gpu Brand'].unique())
+gpu = st.selectbox('GPU', train['Gpu Brand'].unique())
 
-os = st.selectbox('OS',train['Os'].unique())
+os = st.selectbox('OS', train['Os'].unique())
 
 if st.button('Predict Price'):
 
